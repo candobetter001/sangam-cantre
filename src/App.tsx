94458eeef96f6b561f17ed51d89e3ffed1168a0b
Abhi_4889 <candobetter001@gmail.com>
2026-05-20 11:40:04 +0530

    Dt-20-5-26/Wire 15 new routes into App

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,21 @@ import Contact from "./pages/Contact";
 import Awards from "./pages/Awards";
 import Podcast from "./pages/Podcast";
 import AnnualReport from "./pages/AnnualReport";
+import Team from "./pages/Team";
+import FAQ from "./pages/FAQ";
+import PressKit from "./pages/PressKit";
+import Privacy from "./pages/Privacy";
+import Terms from "./pages/Terms";
+import Cookies from "./pages/Cookies";
+import CodeOfConduct from "./pages/CodeOfConduct";
+import Accessibility from "./pages/Accessibility";
+import Diversity from "./pages/Diversity";
+import OpenData from "./pages/OpenData";
+import Sitemap from "./pages/Sitemap";
+import Volunteer from "./pages/Volunteer";
+import SpeakerRequest from "./pages/SpeakerRequest";
+import Collaborate from "./pages/Collaborate";
+import Bookmarks from "./pages/Bookmarks";
 import NotFound from "./pages/NotFound";
 import ScrollToTop from "./components/Scroll";
 
@@ -31,6 +46,7 @@ const App = () => (
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/team" element={<Team />} />
             <Route path="/focus-areas" element={<FocusAreas />} />
             <Route path="/events" element={<Events />} />
             <Route path="/publications" element={<Publications />} />
@@ -40,6 +56,20 @@ const App = () => (
             <Route path="/awards" element={<Awards />} />
             <Route path="/podcast" element={<Podcast />} />
             <Route path="/annual-report" element={<AnnualReport />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/press-kit" element={<PressKit />} />
+            <Route path="/volunteer" element={<Volunteer />} />
+            <Route path="/speaker-request" element={<SpeakerRequest />} />
+            <Route path="/collaborate" element={<Collaborate />} />
+            <Route path="/bookmarks" element={<Bookmarks />} />
+            <Route path="/sitemap" element={<Sitemap />} />
+            <Route path="/privacy" element={<Privacy />} />
+            <Route path="/terms" element={<Terms />} />
+            <Route path="/cookies" element={<Cookies />} />
+            <Route path="/code-of-conduct" element={<CodeOfConduct />} />
+            <Route path="/accessibility" element={<Accessibility />} />
+            <Route path="/diversity" element={<Diversity />} />
+            <Route path="/open-data" element={<OpenData />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
